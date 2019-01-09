ec38845dbe8cc88ded2760a5a3f9100b756a88c7
Bobby Lee <rdlee8181@gmail.com>
2019-01-08 19:33:08 -0800

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,49 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = ({ siteTitle, menu }) => (
+  <div
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+      <nav>
+        {menu.map(item => (
+          <Link key={item.object_id} to={`/${item.object_slug}`}>
+            {item.title}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  </div>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
