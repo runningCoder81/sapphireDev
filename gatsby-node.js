7db98aed35f5295bc55ab5a1de6b3f21dc2b3137
Bobby Lee <rdlee8181@gmail.com>
2019-01-19 16:09:13 -0800

    Fix build error

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,57 +1,57 @@
-const path = require('path')
+// const path = require('path')
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
-  return new Promise((resolve, reject) => {
-    graphql(
-      `
-        {
-          allWordpressPage {
-            edges {
-              node {
-                id
-                slug
-                title
-                content
-              }
-            }
-          }
-          allWordpressPost {
-            edges {
-              node {
-                slug
-              }
-            }
-          }
-        }
-      `
-    ).then(result => {
-      if (result.errors) {
-        console.log(result.errors)
-        reject(result.errors)
-      }
+// exports.createPages = ({ graphql, boundActionCreators }) => {
+//   const { createPage } = boundActionCreators
+//   return new Promise((resolve, reject) => {
+//     graphql(
+//       `
+//         {
+//           allWordpressPage {
+//             edges {
+//               node {
+//                 id
+//                 slug
+//                 title
+//                 content
+//               }
+//             }
+//           }
+//           allWordpressPost {
+//             edges {
+//               node {
+//                 slug
+//               }
+//             }
+//           }
+//         }
+//       `
+//     ).then(result => {
+//       if (result.errors) {
+//         console.log(result.errors)
+//         reject(result.errors)
+//       }
 
-      result.data.allWordpressPage.edges.forEach(({ node }) => {
-        createPage({
-          path: node.slug,
-          component: path.resolve('./src/templates/page.js'),
-          context: {
-            slug: node.slug,
-          },
-        })
-      })
+//       result.data.allWordpressPage.edges.forEach(({ node }) => {
+//         createPage({
+//           path: node.slug,
+//           component: path.resolve('./src/templates/page.js'),
+//           context: {
+//             slug: node.slug,
+//           },
+//         })
+//       })
 
-      result.data.allWordpressPost.edges.forEach(({ node }) => {
-        createPage({
-          path: `posts/${node.slug}`,
-          component: path.resolve('./src/templates/post.js'),
-          context: {
-            slug: node.slug,
-          },
-        })
-      })
+//       result.data.allWordpressPost.edges.forEach(({ node }) => {
+//         createPage({
+//           path: `posts/${node.slug}`,
+//           component: path.resolve('./src/templates/post.js'),
+//           context: {
+//             slug: node.slug,
+//           },
+//         })
+//       })
 
-      resolve()
-    })
-  })
-}
+//       resolve()
+//     })
+//   })
+// }
